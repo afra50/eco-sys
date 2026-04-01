c01321dcb4d45e4bdcf9b92cd597bb53eb391014
Julia Kowalczyk <jkowalczyk505@gmail.com>
2026-04-01 20:10:54 +0200

    Nowe sekcje home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import Hero from "../components/home/Hero";
 import Ecosystem from "../components/home/Ecosystem";
+import Process from "../components/home/Process";
+import Brands from "../components/home/Brands";
+import RecentProjects from "../components/home/RecentProjects";
 
 const Home = () => {
-	return (
-		<main>
-			<Hero />
-			<Ecosystem />
-			{/* Tutaj będą kolejne sekcje, np. <Categories /> */}
-		</main>
-	);
+  return (
+    <main>
+      <Hero />
+      <Brands /> {/* Fajnie wygląda od razu pod Hero jako "social proof" */}
+      <Ecosystem />
+      <Process />
+      <RecentProjects />
+    </main>
+  );
 };
 
 export default Home;
